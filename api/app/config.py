8ede851f6ca853400fcd42b52f02a8f264302d6d
Euclécio Josias Rodrigues <euclecio.dev@gmail.com>
2026-07-26 21:54:36 -0300

    Hardcoded values (PoC)

--- api/app/config.py
+++ api/app/config.py
@@ -5,9 +5,9 @@ class Settings(BaseSettings):
     model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8")
 
     app_name: str = "movies-api"
-    environment: str = "local"
+    environment: str = "staging"
     log_level: str = "INFO"
-    database_url: str = "postgresql+asyncpg://movies:movies@localhost:5432/movies"
+    database_url: str = "postgresql+asyncpg://movies:pg1234@postgres:5432/movies"
 
 
 settings = Settings()
